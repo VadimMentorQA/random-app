7972daa9d5555983bb2c88997ee360fc8def57a0
v.savchenko <v.savchenko@polytech.software>
2024-03-04 11:46:35 +0200

    Update quotes in the script.js file to incorporate Ukrainian proverbs and sayings

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 document.getElementById("quoteButton").addEventListener("click", function () {
   const quotes = [
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-    "The way to get started is to quit talking and begin doing. - Walt Disney",
-    "Life is what happens when you're busy making other plans. - John Lennon",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-    "It is during our darkest moments that we must focus to see the light. - Aristotle",
-    // Add more quotes as desired
+    "Сім раз почитай - один раз тайпінг спід.",
+    "Свій тайпінг спід ближче до гугл-доку.",
+    "Готуй сані літом, а тайпінг спід - до офферу.",
+    "Скажи мені свій тайпінг спід, і я скажу хто ти.",
+    "З клави по кнопці - мені тайпінг спід (з миру по нитці - голому сорочка).",
+    "Кожна жаба свій тайпінг спід хвалить.",
+    "Любиш кататись - люби і тайпінг спід заповняти.",
+    "Баба з возу - кобила за тайпінг спід.",
+    "Без труда не витягнеш і тайпінг спід із дна.",
+    "Скільки вовка не корми, а в студента мейт тайпінг спід більше.",
+    "В тихому болоті - тайпінг спід водиться.",
+    "В умілого в руках і долото тайпінг спід заповнить.",
+    "Дарованого коня за тайпінг спід не садять.",
+    "Скажи мені хто твій друг - і обидва за тайпінг спід!",
+    "Семеро одного на тайпінг спід не ждуть.",
+    "Можна все на світі забувати, сину. Забути не можна тільки тайпінг спід кожної пт.",
   ];
 
   const randomIndex = Math.floor(Math.random() * quotes.length);
